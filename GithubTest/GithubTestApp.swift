10930a0ef2d9a22dad0911904917d69c9df533da
Wafi Choudhury <wafster1337@gmail.com>
2021-07-18 01:38:27 -0500

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Wafi Choudhury on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
